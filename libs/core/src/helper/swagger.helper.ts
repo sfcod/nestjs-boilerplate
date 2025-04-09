569fc198296ef0a0b7f002c864ab53703fb81798
Bozhen Novak <bozhen.novak@zfort.com>
2025-04-09 19:02:10 +0300

    feat: protect swagger endpoint

--- libs/core/src/helper/swagger.helper.ts
+++ libs/core/src/helper/swagger.helper.ts
@@ -1,5 +1,23 @@
 import { resolve } from 'path';
 import { GlobOptionsWithFileTypesFalse, sync } from 'glob';
+import { INestApplication } from '@nestjs/common';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
+
+type SetupSwaggerOptions = { path: string; document: OpenAPIObject; secured?: boolean };
+export const setupSwagger = async (app: INestApplication, { path, document, secured = true }: SetupSwaggerOptions) => {
+    if (secured) {
+        const user = process.env.SWAGGER_USER || 'swagger';
+        const pass = process.env.SWAGGER_PASSWORD || 'swagger';
+
+        app.use(
+            [`${path}/?$`, `${path}-yaml/?`, `${path}-json/?`],
+            basicAuth({ challenge: true, users: { [user]: pass } }),
+        );
+    }
+
+    SwaggerModule.setup(path, app, document);
+};
 
 export const names = async (globPattern: string, options?: GlobOptionsWithFileTypesFalse) => {
     const all = await Promise.all(
